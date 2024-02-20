bf01fed5fec984b0986a0a0261c6f2e0a9e87b9e
lolers <adithyajayan@gmail.com>
2024-02-21 01:17:15 +0530

    C3: Button

---
+++ qtify/src/components/Logo/Logo.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import qtifyLogo from "../../assets/qtify-logo.png";
+const Logo = () => {
+  return <img src={qtifyLogo} alt={"QTify logo"} width={65}></img>;
+};
+
+export default Logo;
