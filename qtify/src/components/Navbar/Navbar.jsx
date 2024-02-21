0ebbfe5a916f73e6d2be313088dccd62e735d1e4
lolers <adithyajayan@gmail.com>
2024-02-21 12:29:38 +0530

    Modified button to be made from scratch with css modules

--- qtify/src/components/Navbar/Navbar.jsx
+++ qtify/src/components/Navbar/Navbar.jsx
@@ -1,27 +1,15 @@
 import React from "react";
 import styles from "./Navbar.module.css";
 import Logo from "../Logo/Logo";
-import Button from "@mui/material/Button";
 import Searchbar from "../Searchbar/Searchbar";
-
+import Buttonn from "../Button/Button";
 export const Navbar = () => {
   return (
     <nav className={styles.Navbar}>
       <Logo></Logo>
       <Searchbar />
-      <Button
-        sx={{
-          backgroundColor: "#121212",
-          color: "#34C94B",
-          borderRadius: "12px",
-          width: "170px",
-          fontSize: "18px",
-          fontWeight: 600,
-          textTransform: "none",
-        }}
-      >
-        Give Feedback
-      </Button>
+      
+      <Buttonn text={"Give Feedback"}></Buttonn>
     </nav>
   );
 };
