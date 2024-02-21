ab321ba2896a0e6fbb39ccfce5fcedbd31f2c9a7
lolers <adithyajayan@gmail.com>
2024-02-21 13:12:20 +0530

    added corrections

--- qtify/src/components/Navbar/Navbar.jsx
+++ qtify/src/components/Navbar/Navbar.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import styles from "./Navbar.module.css";
-import Logo from "../Logo/Logo";
+import Logo from "../../assets/qtify-logo.png"
 import Searchbar from "../Searchbar/Searchbar";
 import Buttonn from "../Button/Button";
 export const Navbar = () => {
   return (
     <nav className={styles.Navbar}>
-      <Logo></Logo>
+      <div className={styles.logoDiv}><img  src={Logo} alt="logo" width={67}/></div>
+      {/* <Logo /> */}
       <Searchbar />
       
       <Buttonn text={"Give Feedback"}></Buttonn>
