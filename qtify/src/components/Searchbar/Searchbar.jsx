ec43cda3761b1bf57960b8d9e0ea53a0bb1511bd
lolers <adithyajayan@gmail.com>
2024-02-21 15:30:11 +0530

    added cursor pointer to all buttons

--- qtify/src/components/Searchbar/Searchbar.jsx
+++ qtify/src/components/Searchbar/Searchbar.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
-import styles from "./Searchbar.module.css"
-import { ReactComponent as SearchIcon } from "../../assets/Search icon.svg"
+import React from "react";
+import styles from "./Searchbar.module.css";
+import { ReactComponent as SearchIcon } from "../../assets/Search icon.svg";
+
 const Searchbar = () => {
   return (
-    <div className={styles.searchBar}><input className={styles.searchInput}type='text' placeholder='Search a song of your choice'></input><button className={styles.searchButton}><SearchIcon /></button></div>
-  )
-}
+    <div className={styles.searchBar}>
+      <input
+        className={styles.searchInput}
+        type="text"
+        placeholder="Search a song of your choice"
+      ></input>
+      <button className={styles.searchButton}>
+        <SearchIcon />
+      </button>
+    </div>
+  );
+};
 
-export default Searchbar
+export default Searchbar;
+
